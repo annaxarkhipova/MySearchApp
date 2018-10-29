43699957085ded6174ecc2bc3543708a86604409
annaxarkhipova <44312251+annaxarkhipova@users.noreply.github.com>
2018-10-29 05:54:07 +0300

    Revision the code

--- search_interface.py
+++ search_interface.py
@@ -2,57 +2,40 @@
 from tkinter import *
 import webbrowser
 
-#создание класса,
-class Application(Frame):
-     def __init__(self, master):
-          super(Application, self).__init__(master)
-          self.grid()
-          self.create_widgets()
-     
-#поле для ввода текста
-          self.search_query = StringVar()
-          self.search_ent = Entry(self)
-          self.search_ent.grid(row=2,column=3, padx=70, pady=0)
-
-          url= "https://google.ru/search?q="
-
-     def openweb():
-          if browser_id.get() == 1:
-               webbrowser.get(using='Chrome').open(url+search_query.get(), new=1)
-               pass
-          elif browser_id.get() == 2:
-               webbrowser.get(using='Safari').open(url+search_query.get(), new=1)
-               pass
-          else:
-               webbrowser.get(using='default').open(url+search_query.get(), new=1)
-               pass
-          
-#создание кнопок
-     def create_widgets(self): 
-
-          self.btn = Button(self, text = "Search",
-             command=openweb             ,
-             padx=40, pady=10,
-             bg="white",fg="pink")
-          self.btn.grid(row=4, column=0, padx = 20, pady = 0)
-
-#выбор браузера
-          browser_id = IntVar()
-          self.chrome_but = Radiobutton(text="Chrome",
-                                                value=1, variable=browser_id)
-          self.chrome_but.grid(row=3, column=1, sticky=W,
-                                       padx = 0, pady = 0)
-
-          self.safari_but = Radiobutton(text="Safari",
-                                                value=0, variable=browser_id)
-          self.safari_but.grid(row=4, column=1, sticky=W,
-                                       padx = 0, pady = 0)
-       
-# main
+#Создание рамки
 root = Tk()
-root.title("Search")
-root.geometry("470x150")
+root.title('Search')
+root.geometry('300x100')
 
-app = Application(root)
+#Создание поискового поля
+search_query = StringVar()
+search_entry = Entry(textvariable=search_query)
+search_entry.grid(row=1, column=1)
+
+#Условия при выборе кнопок браузера
+#pass - исключение других условий
+url = "https://www.google.ru/search?q="
+
+def openweb():
+  if browser_id.get() == 1:
+    webbrowser.get(using='chrome').open(url+search_query.get(), new=1)
+    pass
+  elif browser_id.get() == 2:
+    webbrowser.get(using='safari').open(url+search_query.get(), new=1)
+    pass
+
+#Настройка кнопки поиска и кнопок браузера
+btn = Button(text = "Search", command=openweb, bg="white", fg="pink")
+btn.grid(row=2, column=1, rowspan=2)
+
+browser_id = IntVar()
+browser_id.set() #выбор бразуера по дефолту
+
+chrome_checkbutton = Radiobutton(text="Chrome", value=1, variable=browser_id)
+chrome_checkbutton.grid(row=2, column=2, sticky=W)
+ 
+safari_checkbutton = Radiobutton(text="Safari", value=2, variable=browser_id)
+safari_checkbutton.grid(row=3, column=2, sticky=W)
+
 
 root.mainloop()
